b5c8cb00735b9ecbddd68f70b50eb2e067fd7420
tatsumi <tatsumi071111@gmail.com>
2022-08-03 10:08:27 +0900

    second

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Hello World');
+
+//分割代入
+const myProfile = {
+    name: "蓮人",
+    age: 8,
+};
+
+const {name, age} = myProfile;
+const message1 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message1);
+
